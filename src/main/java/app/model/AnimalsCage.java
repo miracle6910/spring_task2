164cdaaa2a7b4c5cfa84cd43fbdbda108d9c67d8
”miracle” <craptysk@gmail.com>
2024-12-26 14:27:08 +0700

    done

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,25 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+
     @Autowired
+    @Qualifier("dog")
+
     private Animal animal;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+
+    public Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
